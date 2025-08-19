9bc3995633794485dd7a389ea3ccc56eef64055d
theZilber <zilberlex@gmail.com>
2025-08-19 21:08:20 +0200

    Fixed current docs HOPEFULLY

--- svelte.config.js
+++ svelte.config.js
@@ -11,12 +11,7 @@ const config = {
 		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
 		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
 		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
-		adapter: adapter({
-			pages: 'docs',
-			assets: 'docs',
-			fallback: undefined, // or '404.html' if you need SPA fallback
-			precompress: false
-		}),
+		adapter: adapter(),
 		paths: {
 			base: '/svelte-transition-composition'
 		}
